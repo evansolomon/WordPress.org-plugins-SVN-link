a5d5c8215967522a7dbc6d20fb88d9ae2e7b4a39
Aaron Jorbin <aaron@jorb.in>
2017-03-28 11:34:51 -0400

    Adjust style for new plugin directory
    WordPress.org updated it's plugin directory design. This makes a few updates for styles so that the button is still just as wonderful as it once ones.
    
    Now uses the "button" and "button-large" classes used on the page  on the link so the style should be a bit more persistant.

--- script.js
+++ script.js
@@ -14,12 +14,11 @@ if( plugin_slug && plugin_slug[1] && plugin_slug[1] != 'tags' ) {
 	var svnLink = 'http://plugins.svn.wordpress.org/' + plugin_slug[1] + '/';
 	var anchor = document.createElement('a');
 	anchor.href=svnLink;
+	anchor.classname = 'button button-large';
 	anchor.appendChild( document.createTextNode('SVN Repository') );
 
 	// build our paragraph
 	var paragraph = document.createElement('p');
-	paragraph.className='button';
-	paragraph.style.background = '#21759b';
 	paragraph.style.marginTop = '5px';
 	paragraph.appendChild(anchor);
 
